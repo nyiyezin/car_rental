88051e9ee0d38501f97fa3143132111c6dc389a7
nyiyezin <nyiyezin11@gmail.com>
2024-11-28 16:14:12 +0630

    fix: Add redirect to dashboard page after login

--- app/Http/Controllers/Auth/SessionsController.php
+++ app/Http/Controllers/Auth/SessionsController.php
@@ -29,7 +29,7 @@ class SessionsController extends Controller
 
         session()->regenerate();
 
-        return redirect('/')->with('success', 'Welcome Back!');
+        return redirect()->route('adminDashboard')->with('success', 'Welcome Back!');
     }
 
 
